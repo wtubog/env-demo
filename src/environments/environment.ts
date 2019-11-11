8a6ebd22ccb11fe31434c04b3aaee30dfaac41dc
Wilco B. Tubog <wilco.tubog@admins-iMac-2.local>
2019-11-11 11:48:44 +0800

    initial demo commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  whereAmI:
+    'Development stage... because the "dev" stage should be the default environment when you are developing locally'
 };
 
 /*
